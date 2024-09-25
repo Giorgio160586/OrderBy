9d64d642a73e245dceb5752c8c1c7370d25c877a
Giorgio <giorgio.saad@swisslog-healthcare.com>
2024-09-25 08:57:49 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<string>() {
+                    "Y",
+                    "A",
+                    "F",
+                    "​Z"
+                };
+
+            list = list.OrderBy(x => x).ToList();
+
+            list.ForEach(x => Debug.Print(x));
+        }
+    }
+}
